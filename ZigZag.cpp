be49ef2b488be2f5c373e290a3454bedb16fd25c
wenxizhabg <162524291+wenxizhabg@users.noreply.github.com>
2024-03-17 08:49:43 +0700

    40 lines

--- ZigZag.cpp
+++ ZigZag.cpp
@@ -1,69 +1,38 @@
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 using namespace std;
 
-void dichuyen(bool up, int &row, int &col) {
-    if (up) {
-        row--;
-        col--;
-    }
-    else {
-        row++;
-        col++;
-    }
-}
-
 int main () {
-    ifstream in ("ZigZag.inp");
-    ofstream out ("ZigZag.out");
-
     int n, s; cin >> n >> s;
     int maTran [n][n];
-    bool up;
+    int x, y;
+    ofstream out ("ZigZag.out");
 
-    int bienTrai = 0, bienPhai = n - 1;
-    int bienTren = 0, bienDuoi = n - 1;
-
-    int row = bienDuoi, col = bienTrai;
-    maTran [row][col] = s;
-
-    while (true) {
-        if (row == bienDuoi) {
-            up = true;
-            col++;
-            if (col > bienPhai) dichuyen(up, row, col);
-            maTran [row][col] = ++s;
-        } 
-        else if (col == bienPhai) {
-            up = true;
-            row --;
-            maTran [row][col] = ++s;
+    for (int duongCheo = - (n-1); duongCheo <= (n-1); duongCheo++) {
+        int doLonDuongCheo = (duongCheo > 0) ? duongCheo : -duongCheo;
+        int huong ;
+        if ( (duongCheo + n - 1) % 2 == 0) { //di xuoong
+            x = doLonDuongCheo;
+            y = 0;
+            huong = 1;
         }
-
-        if (col ==  bienTrai) {
-            up = false;
-            row --;
-            if (row < bienTren) dichuyen(up, row, col);
-            maTran [row][col] = ++s;
+        else {
+            x = n - 1;
+            y = x - doLonDuongCheo;
+            huong = -1;
         }
-        else if (row == bienTren) {
-            up = false;
-            col ++;
-            maTran [row][col] = ++s;
+        
+        for (int i = 0; i < n - doLonDuongCheo; i++) {
+            if (duongCheo < 0) maTran [x + i * huong][y + i * huong] = s++;
+            else maTran [y + i * huong][x + i * huong] = s++;
         }
-
-        if (row == bienTren && col == bienPhai) break;
-
-        dichuyen(up, row, col);
-        maTran [row][col] = ++s;     
     }
     
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) cout << maTran [i][j] << "  ";
-        cout << "\n";
+        for (int j = 0; j < n; j++) out << setw(3) << maTran[i][j] << " ";
+        out << endl;
     }
     
-    in.close();
-    out.close();
     return 0;
 }

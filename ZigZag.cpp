433557cf34e501a2b416b59ac18cedaa348eefcb
Trương Nhật Hoàng <162524291+wenxizhabg@users.noreply.github.com>
2024-03-18 10:26:24 +0700

    Update ZigZag.cpp

--- ZigZag.cpp
+++ ZigZag.cpp
@@ -1,38 +1,35 @@
-#include <iostream>
 #include <fstream>
-#include <iomanip>
 using namespace std;
 
+struct vecto {
+    int x, y, huong;
+};
+
 int main () {
-    int n, s; cin >> n >> s;
-    int maTran [n][n];
-    int x, y;
+    ifstream in ("ZigZag.inp");
     ofstream out ("ZigZag.out");
+
+    int n, s; in >> n >> s;
+    int maTran [n][n];
+    vecto vec;
 
     for (int duongCheo = - (n-1); duongCheo <= (n-1); duongCheo++) {
         int doLonDuongCheo = (duongCheo > 0) ? duongCheo : -duongCheo;
-        int huong ;
-        if ( (duongCheo + n - 1) % 2 == 0) { //di xuoong
-            x = doLonDuongCheo;
-            y = 0;
-            huong = 1;
-        }
-        else {
-            x = n - 1;
-            y = x - doLonDuongCheo;
-            huong = -1;
-        }
+
+        if ((duongCheo + n - 1) % 2 == 0) vec = {doLonDuongCheo, 0, 1};
+        else vec = {n - 1, n - 1 - doLonDuongCheo, -1};
         
-        for (int i = 0; i < n - doLonDuongCheo; i++) {
-            if (duongCheo < 0) maTran [x + i * huong][y + i * huong] = s++;
-            else maTran [y + i * huong][x + i * huong] = s++;
-        }
+        for (int i = 0; i < n - doLonDuongCheo; i++)
+            if (duongCheo < 0) maTran [vec.x + i * vec.huong][vec.y + i * vec.huong] = s++;
+            else maTran [vec.y + i * vec.huong][vec.x + i * vec.huong] = s++;
     }
     
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) out << setw(3) << maTran[i][j] << " ";
+        for (int j = 0; j < n; j++) out << maTran[i][j] << " ";
         out << endl;
     }
     
+    in.close();
+    out.close();
     return 0;
 }
